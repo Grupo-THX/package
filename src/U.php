291309a18423099853f71eb768fe3e41037b8821
deljdl <deljdl@gmail.com>
2023-06-18 19:07:14 -0300

    remove + senderrors

--- src/U.php
+++ src/U.php
@@ -29,111 +29,99 @@ class U{
 
     static function valorPorExtenso($valor, $bolExibirMoeda = true, $bolPalavraFeminina = false)
 	{
-		try
-		{
-				$valor = number_format($valor, 2, ".", ".");
-				$singular = null;
-				$plural = null;
+		
+        $valor = number_format($valor, 2, ".", ".");
+        $singular = null;
+        $plural = null;
 
-				if ( $bolExibirMoeda )
-				{
-						$singular = array("centavo", "real", "mil", "milhão", "bilhão", "trilhão", "quatrilhão");
-						$plural = array("centavos", "reais", "mil", "milhões", "bilhões", "trilhões","quatrilhões");
-				}
-				else
-				{
-						$singular = array("", "", "mil", "milhão", "bilhão", "trilhão", "quatrilhão");
-						$plural = array("", "", "mil", "milhões", "bilhões", "trilhões","quatrilhões");
-				}
+        if ( $bolExibirMoeda )
+        {
+                $singular = array("centavo", "real", "mil", "milhão", "bilhão", "trilhão", "quatrilhão");
+                $plural = array("centavos", "reais", "mil", "milhões", "bilhões", "trilhões","quatrilhões");
+        }
+        else
+        {
+                $singular = array("", "", "mil", "milhão", "bilhão", "trilhão", "quatrilhão");
+                $plural = array("", "", "mil", "milhões", "bilhões", "trilhões","quatrilhões");
+        }
 
-				$c = array("", "cem", "duzentos", "trezentos", "quatrocentos","quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos");
-				$d = array("", "dez", "vinte", "trinta", "quarenta", "cinquenta","sessenta", "setenta", "oitenta", "noventa");
-				$d10 = array("dez", "onze", "doze", "treze", "quatorze", "quinze","dezesseis", "dezessete", "dezoito", "dezenove");
-				$u = array("", "um", "dois", "três", "quatro", "cinco", "seis","sete", "oito", "nove");
-
-
-				if ( $bolPalavraFeminina )
-				{
-
-						if ($valor == 1)
-						{
-							$u = array("", "uma", "duas", "três", "quatro", "cinco", "seis","sete", "oito", "nove");
-						}
-						else
-						{
-							$u = array("", "um", "duas", "três", "quatro", "cinco", "seis","sete", "oito", "nove");
-						}
+        $c = array("", "cem", "duzentos", "trezentos", "quatrocentos","quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos");
+        $d = array("", "dez", "vinte", "trinta", "quarenta", "cinquenta","sessenta", "setenta", "oitenta", "noventa");
+        $d10 = array("dez", "onze", "doze", "treze", "quatorze", "quinze","dezesseis", "dezessete", "dezoito", "dezenove");
+        $u = array("", "um", "dois", "três", "quatro", "cinco", "seis","sete", "oito", "nove");
 
 
-						$c = array("", "cem", "duzentas", "trezentas", "quatrocentas","quinhentas", "seiscentas", "setecentas", "oitocentas", "novecentas");
+        if ( $bolPalavraFeminina )
+        {
+
+                if ($valor == 1)
+                {
+                    $u = array("", "uma", "duas", "três", "quatro", "cinco", "seis","sete", "oito", "nove");
+                }
+                else
+                {
+                    $u = array("", "um", "duas", "três", "quatro", "cinco", "seis","sete", "oito", "nove");
+                }
 
 
-				}
+                $c = array("", "cem", "duzentas", "trezentas", "quatrocentas","quinhentas", "seiscentas", "setecentas", "oitocentas", "novecentas");
 
 
-				$z = 0;
+        }
 
-				$inteiro = explode( ".", $valor );
 
-				for ( $i = 0; $i < count( $inteiro ); $i++ )
-				{
-						for ( $ii = mb_strlen( $inteiro[$i] ); $ii < 3; $ii++ )
-						{
-							$inteiro[$i] = "0" . $inteiro[$i];
-						}
-				}
+        $z = 0;
 
-				// $fim identifica onde que deve se dar junção de centenas por "e" ou por "," ;)
-				$rt = null;
-				$fim = count( $inteiro ) - ($inteiro[count( $inteiro ) - 1] > 0 ? 1 : 2);
-				for ( $i = 0; $i < count( $inteiro ); $i++ )
-				{
-						$valor = $inteiro[$i];
-						$rc = (($valor > 100) && ($valor < 200)) ? "cento" : $c[$valor[0]];
-						$rd = ($valor[1] < 2) ? "" : $d[$valor[1]];
-						$ru = ($valor > 0) ? (($valor[1] == 1) ? $d10[$valor[2]] : $u[$valor[2]]) : "";
+        $inteiro = explode( ".", $valor );
 
-						$r = $rc . (($rc && ($rd || $ru)) ? " e " : "") . $rd . (($rd && $ru) ? " e " : "") . $ru;
-						$t = count( $inteiro ) - 1 - $i;
-						$r .= $r ? " " . ($valor > 1 ? $plural[$t] : $singular[$t]) : "";
-						if ( $valor == "000")
-							$z++;
-						elseif ( $z > 0 )
-							$z--;
+        for ( $i = 0; $i < count( $inteiro ); $i++ )
+        {
+                for ( $ii = mb_strlen( $inteiro[$i] ); $ii < 3; $ii++ )
+                {
+                    $inteiro[$i] = "0" . $inteiro[$i];
+                }
+        }
 
-						if ( ($t == 1) && ($z > 0) && ($inteiro[0] > 0) )
-							$r .= ( ($z > 1) ? " de " : "") . $plural[$t];
+        // $fim identifica onde que deve se dar junção de centenas por "e" ou por "," ;)
+        $rt = null;
+        $fim = count( $inteiro ) - ($inteiro[count( $inteiro ) - 1] > 0 ? 1 : 2);
+        for ( $i = 0; $i < count( $inteiro ); $i++ )
+        {
+                $valor = $inteiro[$i];
+                $rc = (($valor > 100) && ($valor < 200)) ? "cento" : $c[$valor[0]];
+                $rd = ($valor[1] < 2) ? "" : $d[$valor[1]];
+                $ru = ($valor > 0) ? (($valor[1] == 1) ? $d10[$valor[2]] : $u[$valor[2]]) : "";
 
-						if ( $r )
-							$rt = $rt . ((($i > 0) && ($i <= $fim) && ($inteiro[0] > 0) && ($z < 1)) ? ( ($i < $fim) ? ", " : " e ") : " ") . $r;
-				}
+                $r = $rc . (($rc && ($rd || $ru)) ? " e " : "") . $rd . (($rd && $ru) ? " e " : "") . $ru;
+                $t = count( $inteiro ) - 1 - $i;
+                $r .= $r ? " " . ($valor > 1 ? $plural[$t] : $singular[$t]) : "";
+                if ( $valor == "000")
+                    $z++;
+                elseif ( $z > 0 )
+                    $z--;
 
-				$rt = mb_substr( $rt, 1 );
+                if ( ($t == 1) && ($z > 0) && ($inteiro[0] > 0) )
+                    $r .= ( ($z > 1) ? " de " : "") . $plural[$t];
 
-				return($rt ? trim( $rt ) : "zero");
-		}
-		catch( Exception $e )
-		{
-			X::sendErrors($e->getMessage(), __CLASS__.'>'.__FUNCTION__.'>'.__LINE__);
-		}
+                if ( $r )
+                    $rt = $rt . ((($i > 0) && ($i <= $fim) && ($inteiro[0] > 0) && ($z < 1)) ? ( ($i < $fim) ? ", " : " e ") : " ") . $r;
+        }
+
+        $rt = mb_substr( $rt, 1 );
+
+        return($rt ? trim( $rt ) : "zero");
+
 	}
 
     static function urlAtual($remove_GET = false)
 	{
-		try
-		{
-			$protocolo =  isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? "https://" : 'http://';
-			$retorno = $protocolo.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-			if($remove_GET)
-			{
-				$urlParts = explode('?', $retorno);
-				return $urlParts[0];
-			}
-			return $retorno;
-		}
-		catch( Exception $e )
-		{
-			X::sendErrors($e->getMessage(), __CLASS__.'>'.__FUNCTION__.'>'.__LINE__);
-		}
+        $protocolo =  isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? "https://" : 'http://';
+        $retorno = $protocolo.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+        if($remove_GET)
+        {
+            $urlParts = explode('?', $retorno);
+            return $urlParts[0];
+        }
+        return $retorno;
 	}
 }
